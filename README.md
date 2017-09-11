cbbb49871b0076b3bf2cca5c724ecfe89f29c807
Giovanni Merlino <gmerlino@unime.it>
2017-09-11 13:33:31 +0200

    Import

---
+++ README.md
@@ -0,0 +1,12 @@
+# Homebrew formula for Exfat
+## How do I install this formula?
+`brew tap gmerlino/exfat` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/gmerlino/homebrew-exfat/master/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://github.com/Homebrew/brew/tree/master/share/doc/homebrew#readme).
---
+++ exfat.rb
@@ -0,0 +1,24 @@
+class Exfat < Formula
+  desc "Free exFAT file system implementation"
+  homepage "https://github.com/relan/exfat"
+
+  head do
+    url "https://github.com/relan/exfat.git"
+
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on 'pkg-config' => :build
+  end
+
+  depends_on :macos => :mavericks
+
+  def install
+    system "autoreconf", "-fiv"
+    ENV.append_path "PKG_CONFIG_LIBDIR", "#{HOMEBREW_PREFIX}/lib/pkgconfig"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
